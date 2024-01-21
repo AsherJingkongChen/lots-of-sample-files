536e020bf2c698eed943eef8346e7c3a82544ea2
AsherJingkongChen <gpks556@gmail.com>
2024-01-22 03:54:53 +0800

    (dev), (enhance): Add script to create sample from the template

---
+++ scripts/create-sample-media-types.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { cp, mkdir } from 'fs/promises';
+
+const { argv } = process;
+if (argv.length < 3) {
+  throw new Error(
+    'Missing required arguments: "Sample media types". ' +
+      `Usage: ${argv[0]} ${argv[1]} <Sample-media-types...>`,
+  );
+}
+
+const mediaTypes = argv.slice(2);
+Promise.all(mediaTypes.map(createSample));
+
+async function createSample(mediaType) {
+  const resourcesDir = join(
+    dirname(fileURLToPath(import.meta.url)),
+    '../resources',
+  );
+  const sampleDir = join(resourcesDir, 'samples', mediaType);
+  const docsDir = join(sampleDir, 'docs');
+  const filesDir = join(sampleDir, 'files');
+  const templateDir = join(resourcesDir, 'archives/docs/template');
+
+  await mkdir(filesDir, { recursive: true });
+  await cp(templateDir, docsDir, { recursive: true });
+}
